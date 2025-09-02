4b655a843ad8a92549612bec5bc88809d95e6f46
Lunnate <sleepyyme@mail.ru>
2025-09-02 22:22:59 +0500

    upd

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import axios from "axios";
 
 interface Chat {
   id: string;
@@ -8,42 +9,89 @@ interface Chat {
 }
 
 interface Message {
-  role: 'user' | 'LLM';
+  role: "user" | "assistant";
   content: string;
 }
 
+const API_KEY: string = import.meta.env.VITE_OPENROUTER_API_KEY;
 const chats = ref<Chat[]>([]);
 
-const currentChat = ref<Chat | null>(null)
-const message = ref<string>('')
-function createNewChat() {
+const currentChat = ref<Chat | null>(null);
+const message = ref<string>("");
+function loadChats():void {
+  const savedChats = localStorage.getItem("chats");
+  if (savedChats) {
+    chats.value = JSON.parse(savedChats);
+  }
+}
+loadChats();
+function createNewChat(): void {
   const newChat: Chat = {
     id: Date.now().toString(),
-    title: message.value ? message.value : 'Новый чат',
+    title: message.value ? message.value : "Новый чат",
     messages: [],
-  }
-  chats.value.push(newChat)
-  currentChat.value = newChat
+  };
+  chats.value.push(newChat);
+  currentChat.value = newChat;
+  localStorage.setItem("chats", JSON.stringify(chats.value));
 }
 
-function sendMessage() {
-  if (!message.value) return
+function sendMessage(): void {
+  if (!message.value) return;
   if (!currentChat.value) {
-    createNewChat()
+    createNewChat();
   }
   currentChat.value.messages?.push({
-    role: 'user',
-    content: message.value
-  })
-  message.value = ''
-  getAnswer()
+    role: "user",
+    content: message.value,
+  });
+  message.value = "";
+  getAnswer();
+  localStorage.setItem("chats", JSON.stringify(chats.value));
 }
 
-async function getAnswer() {
-  currentChat.value.messages?.push({
-    role: 'LLM',
-    content: 'Ответ от LLM'
-  })
+async function getAnswer(): Promise<void> {
+  try {
+    currentChat.value.messages?.push({
+      role: "assistant",
+      content: "Печатает...",
+    });
+
+    const response = await axios.post(
+      "https://openrouter.ai/api/v1/chat/completions",
+      {
+        model: "deepseek/deepseek-chat-v3.1:free",
+        messages: currentChat.value.messages
+          ?.filter((message) => message.content !== "Печатает...")
+          ?.map((message) => ({
+            role: message.role,
+            content: message.content,
+          })),
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${API_KEY}`,
+          "Content-Type": "application/json",
+          "HTTP-Referer": "http://localhost:5173/",
+          "X-Title": "Your App Name",
+        },
+      },
+    );
+
+    const aiMessage: string = response.data.choices[0].message.content;
+    const lastIndex: number = currentChat.value.messages.length - 1;
+    currentChat.value.messages[lastIndex].content = aiMessage;
+    localStorage.setItem("chats", JSON.stringify(chats.value));
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.log("Error: ", error.message);
+      const lastIndex: number = currentChat.value.messages.length - 1;
+      if (currentChat.value.messages[lastIndex]?.content === "Печатает...") {
+        currentChat.value.messages[lastIndex].content =
+          "Произошла ошибка при получении ответа";
+      }
+    }
+  }
 }
 </script>
 
@@ -59,7 +107,13 @@ async function getAnswer() {
         </button>
       </div>
       <nav class="flex flex-1 flex-col p-4">
-        <a href="#" class="hover:bg-neutral-700 rounded-lg p-2" v-for="(chat, index) in chats" :key="index" @click="currentChat = chat">
+        <a
+          href="#"
+          class="hover:bg-neutral-700 rounded-lg p-2"
+          v-for="(chat, index) in chats"
+          :key="index"
+          @click="currentChat = chat"
+        >
           <span>{{ chat.title }}</span>
         </a>
       </nav>
@@ -87,16 +141,27 @@ async function getAnswer() {
           Чем я могу помочь сегодня?
         </h1>
         <div v-else class="w-full h-full p-4">
-          <div class="flex flex-col" v-for="(message, id) in currentChat.messages" :key="id" >
+          <div
+            class="flex flex-col"
+            v-for="(message, id) in currentChat.messages"
+            :key="id"
+          >
             <div v-if="message.role === 'user'" class="flex justify-end">
-              <p class="p-2 rounded-lg bg-neutral-800 text-white">{{ message.content }} </p>
+              <p
+                class="p-2 rounded-2xl rounded-br-md bg-blue-600 text-white mb-4 shadow-sm"
+              >
+                {{ message.content }}
+              </p>
             </div>
             <div v-else class="flex justify-start">
-              <p class="p-2 rounded-lg bg-neutral-800 text-white"> {{ message.content }} </p>
+              <p
+                class="mb-4 p-2 rounded-2xl rounded-bl-md bg-neutral-800 text-white shadow-sm"
+              >
+                {{ message.content }}
+              </p>
             </div>
           </div>
         </div>
-
       </div>
 
       <div class="p-4 md:p-6 w-full max-w-4xl mx-auto">
@@ -107,7 +172,10 @@ async function getAnswer() {
             placeholder="Введите ваш запрос здесь..."
             class="w-full border-none rounded-full p-4 bg-neutral-800 focus:outline-none"
           />
-          <button class="absolute text-white right-12 top-4 cursor-pointer" @click="sendMessage">
+          <button
+            class="absolute text-white right-12 top-4 cursor-pointer"
+            @click="sendMessage"
+          >
             Отправить
           </button>
         </div>
